7591831bbf53e362fc98ca2fb4d548c01f5110c2
Eduardo Rabelo <oieduardorabelo@gmail.com>
2020-11-26 02:23:28 +1300

    log

--- main.js
+++ main.js
@@ -8,20 +8,26 @@ let app = express();
 let color = getRandomColor();
 
 app.get("/", (req, res) => {
-  let hostname = childProcess.execSync("curl http://169.254.169.254/latest/meta-data/hostname", { encoding: "utf8" })
-  res.set('Content-Type', 'text/html');
-  res.send(new Buffer(`
+  console.log(process.env);
+  let hostname = childProcess.execSync(
+    "curl http://169.254.169.254/latest/meta-data/hostname",
+    { encoding: "utf8" }
+  );
+  res.set("Content-Type", "text/html");
+  res.send(
+    new Buffer(`
     <html style="background-color:${color};">
       <h1>It works at ${hostname}</h1>
     </html>
-  `));
-})
+  `)
+  );
+});
 
-app.listen(PORT)
+app.listen(PORT);
 
 function getRandomColor() {
-  let letters = '0123456789ABCDEF';
-  let color = '#';
+  let letters = "0123456789ABCDEF";
+  let color = "#";
   for (let i = 0; i < 6; i++) {
     color += letters[Math.floor(Math.random() * 16)];
   }
